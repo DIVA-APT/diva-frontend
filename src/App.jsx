f1aab6bfdbc67cdd3188010dd15d63baa288b1fb
sanghyeok <ish775800@gmail.com>
2025-01-28 20:59:51 +0900

    style: 헤더 스타일 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import { Outlet } from 'react-router';
+import logo1 from './assets/logo-1.webp';
 
 const App = () => {
   return (
     <>
-      <h1 className='col-sm-6'>헤더 입니다. 로고 이미지가 포함된.</h1>
+      <header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          backgroundColor: '#6A9AB0',
+          padding: '5px 10px',
+        }}
+      >
+        <img src={logo1} width={50} height={50} alt='logo'></img>
+        <h1
+          className='col-sm-6'
+          style={{ color: '#001F3F', marginLeft: '10px' }}
+        >
+          DivA
+        </h1>
+      </header>
       <Outlet />
     </>
   );
